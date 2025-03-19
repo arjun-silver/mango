ffa31b994296b3d383bc1f0fb5ad3cb778c3e1cc
arjun-crypto-dev <arjun.sinhala@gmail.com>
2025-03-19 11:58:55 +0700

    added debug info

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+const link = ref("empty_link")
 async function create_invoice_link() {
   const data = await $fetch("/api/tg/create")
+  link.value = data
   window.open(data)
 }
 </script>
 
 <template lang="pug">
 button(@click="create_invoice_link") create
+button(@click="window.open(link)") open {{ link }}
 pre hi!
 </template>
