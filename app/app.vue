f57a5b04f56449abaebc224711fad8832168c84d
arjun-silver <arjun.sinhala@gmail.com>
2025-03-26 10:35:19 +0700

    add richads

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,25 @@ import { TonConnectUIProvider } from "@townsquarelabs/ui-vue"
 const options = {
   manifestUrl: "https://arjun-silver.github.io/mango/ton-connect-manifest.json",
 }
+
+useHead({
+  script: [
+    {
+      src: "https://richinfo.co/richpartners/telegram/js/tg-ob.js",
+      defer: true,
+    },
+    {
+      innerHTML: `
+        window.TelegramAdsController = new TelegramAdsController();
+        window.TelegramAdsController.initialize({
+          pubId: "966020",
+          appId: "1876",
+        });
+      `,
+      type: "text/javascript",
+    },
+  ],
+})
 </script>
 
 <template lang="pug">
